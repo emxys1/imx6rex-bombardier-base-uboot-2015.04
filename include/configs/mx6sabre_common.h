da1025e4878d220fccd1cedfb54074446ed2f188
angel-munoz <ext.angel.munoz@es.transport.bombardier.com>
2016-02-03 15:46:37 +0100

    Set U-Boot delay to 3 secs

--- include/configs/mx6sabre_common.h
+++ include/configs/mx6sabre_common.h
@@ -97,7 +97,7 @@
 #define CONFIG_CMD_SETEXPR
 #undef CONFIG_CMD_IMLS
 
-#define CONFIG_BOOTDELAY               5
+#define CONFIG_BOOTDELAY               3
 
 #define CONFIG_LOADADDR                        0x12000000
 #define CONFIG_SYS_TEXT_BASE           0x17800000
